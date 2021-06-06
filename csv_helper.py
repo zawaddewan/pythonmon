b38d75cfca6dc026c189c80c2c31b9b1f3e4802c
Vokainodragon <xxcreperninjaxx@gmail.com>
2021-06-06 14:56:06 -0400

    made dictionary data for moves and pokemon, added starting menu

--- csv_helper.py
+++ csv_helper.py
@@ -5,32 +5,48 @@ def get_headers(s):
     s = s[:s.find("\n")]
     g = s.split(",")
     return g
-    
+
+def splitcomma(s):
+    newl = list(s.split(','))
+    return newl
+
 def get_data(s):
-    s = s[s.find("\n")+1:]
-    data = s.split("\n")
-    i = 0
-    while i < len(data):
-        data[i] = data[i].split(",")
-        i += 1
+    headerend = s.find('\n')
+    s = s[headerend:]
+    s = s.strip()
+    line = s.split('\n')
+    data = list(map(splitcomma, line))
     return data
-    
+
 def make_dict(data):
     d = {}
-    for x in data:
-        y = x[0]
-        x.remove(y)
-        d[y] = x
+    for lists in data:
+        i = 1
+        values = lists[0]
+        d[values] = []
+        while i < len(lists):
+            d[values].append(int(lists[i]))
+            i += 1
+    return d
+
+def make_dict_moves(data):
+    d = {}
+    for lists in data:
+        i = 1
+        values = lists[0]
+        d[values] = []
+        while i < len(lists):
+            d[values].append(lists[i])
+            i += 1
     return d
 
 def combine_dict(dict, headers):
-    temp_dict = {}
-    for x in dict:
-        g = dict[x]
+    headersnomove = headers[1:]
+    for key in dict.keys():
         i = 0
-        while i < len(g):
-            temp_dict[headers[i]] = g[i]
+        newdict = {}
+        while i < len(headersnomove):
+            newdict[headersnomove[i]] = dict[key][i]
             i += 1
-        year_dict[x] = temp_dict
-        dict = {}
+        dict[key] = newdict
     return dict
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from csv_helper import *
 import time
+from pprint import pprint
 
 def lowercase(s):
     newstring = ''
@@ -15,6 +16,17 @@ def lowercase(s):
 logofile = open('logo2.txt')
 logo = logofile.read()
 
+pokemonlist = ['Charizard', 'Greninja', 'Raichu', 'Lucario', 'Empoleon']
+lowercasepoke = list(map(lowercase, pokemonlist))
+
+def makepokemonchoice(list):
+    i = 0
+    string = ''
+    while i < len(list):
+        string += '> ' + list[i] + '\n'
+        i += 1
+    return string
+
 def gamestart():
     print(logo)
     time.sleep(1)
@@ -29,9 +41,31 @@ def gamestart():
         print('Welcome to the World of Pythonmon!')
         time.sleep(1)
         print('Select your Pokemon:')
-        
+        time.sleep(1)
+        print(makepokemonchoice(pokemonlist))
+        chosenpokemon = ''
+        while chosenpokemon not in lowercasepoke:
+            chosenpokemon = lowercase(input())
+
 gamestart()
-    
+
+movesfile = open('moves.csv')
+moves = movesfile.read()
+movesheader = get_headers(moves)
+movesdata = get_data(moves)
+movesdict = make_dict(movesdata)
+#Below is the final moves data as a dictionary
+movesdictfinal = combine_dict(movesdict, movesheader)
+
+pokemonfile = open('pokemon.csv')
+pokemon = pokemonfile.read()
+pokemonheader = get_headers(pokemon)
+pokemondata = get_data(pokemon)
+pokemondict = make_dict_moves(pokemondata)
+#Below is the final pokemon stats data as a dictionary
+pokemondictfinal = combine_dict(pokemondict, pokemonheader)
+
+
 p1nature = 0 #Later, random number generation from 0-24 will determine natures
 p2nature = 0
 
@@ -66,8 +100,14 @@ def natmodcalc(nature, stat): #Calculates how a stat will be affected by the giv
             return 1.1
         elif (nature == 21 and stat == "atk") or (nature == 22 and stat == "def") or (nature == 22 and stat == "spd") or (nature == 23 and stat == "spatk"):
             return 0.9
-    
+
 def othercalc(base, IV, EV, nature, stat):
     nmod = natmodcalc(nature, stat)
     return ((((2 * base + IV + (EV / 4)) * 50) / 100) + 5) * nmod
-    
+
+def damagecalc(attack, enemydefense, pow, critmod, atkstage):
+    damage = (((((((2 * 50) / 5) + 2) * pow * (attack * (1 + (atkstage / 2)))) / defense ) / 50) + 2) * modifier
+    return
+
+def modifiercalc():
+    return
