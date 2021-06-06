ce70aa761ea5d19ffe1b579aa86882dc2c8f655e
Vokainodragon <70926133+Vokainodragon@users.noreply.github.com>
2021-06-06 12:47:48 -0400

    Add files via upload
    added logo and menu/beginning thing

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
 from csv_helper import *
+import time
 
-print("Welcome to the world of Pythonmon!")
+def lowercase(s):
+    newstring = ''
+    i = 0
+    while i < len(s):
+        if s[i] >= 'A' and s[i] <= 'Z':
+            newstring += chr(ord(s[i]) + 32)
+        else:
+            newstring += s[i]
+        i += 1
+    return newstring
 
+logofile = open('logo2.txt')
+logo = logofile.read()
+
+def gamestart():
+    print(logo)
+    time.sleep(1)
+    print('> Play')
+    print('> Credits')
+    option = lowercase(input())
+    if option ==  'credits':
+        print('Charizard ASCII art: https://www.asciiart.eu/video-games/pokemon')
+        while option != 'play':
+            option = lowercase(input())
+    if option == 'play':
+        print('Welcome to the World of Pythonmon!')
+        time.sleep(1)
+        print('Select your Pokemon:')
+        
+gamestart()
+    
 p1nature = 0 #Later, random number generation from 0-24 will determine natures
 p2nature = 0
 
