0c5839a5c827f8a8974ae72fa28cce9311658176
zawaddewan <zawaddewan@gmail.com>
2021-06-13 12:38:49 -0400

    added titles to graphs, stat changes are now reflected on graphs

--- statcharts.py
+++ statcharts.py
@@ -1,10 +1,16 @@
 import matplotlib.pyplot as plt
+import math
 
 def statcharts(c, p, es, e):
     fig, ax = plt.subplots()
     statlabels = ["Attack", "Defense", "Sp. Atk", "Sp. Def", "Speed"]
-    statlist = [c["Attack"], c["Defense"], c["Sp. Atk"], c["Sp. Def"], c["Speed"]]
+    a = c["Attack"] * max(2, 2 + c["atkstage"])/max(2, 2 - c["atkstage"])
+    d = c["Defense"] * max(2, 2 + c["defstage"])/max(2, 2 - c["defstage"])
+    spa = c["Sp. Atk"] * max(2, 2 + c["spatkstage"])/max(2, 2 + c["spatkstage"])
+    spd = c["Sp. Def"] + max(2, 2 + c["spdefstage"])/max(2, 2 + c["spdefstage"])
+    statlist = [a, d, spa, spd, c["Speed"]]
     plt.barh(statlabels, statlist)
+    plt.title("Pokemon Stats")
     ax.invert_yaxis()
 
 def hpcharts(c, p, es, e):
@@ -12,6 +18,7 @@ def hpcharts(c, p, es, e):
     hplist = [es["HP"], c["HP"]]
     plt.figure(figsize=(12,6))
     plt.barh(hplabels, hplist)
+    plt.title("Health of Pokemon")
     if c["MAXHP"] > es["MAXHP"]:
         plt.xlim([0, int(c["MAXHP"])])
     else:
